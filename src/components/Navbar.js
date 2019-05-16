e014df4657f9f0db0142349bd907bfeb61f2be9a
Benjamin Siegel <benjaminsiegel@Benjamins-MBP-2.home>
2019-05-15 21:58:46 -0400

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import building from './images/building.png'
 import './Navbar.css';
 
 const Navbar = () => {
   return(
     <div className="navbar">
+      <img className="logo" src={building}/>
       <p className="company">StreetHard</p>
     </div>
   )
