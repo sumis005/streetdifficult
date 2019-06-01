3d6e5acd1d906e617a243a638f92386056bc6614
Jeffrey Lancaster <jeffrey.lancaster@gmail.com>
2019-05-31 21:34:42 -0400

    fat arrow and functional constructor

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import DataView from './components/DataView.js'
 import Navbar from './components/Navbar.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <h1 className="header">Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough</h1>
-
-      <DataView
-        borough="Brooklyn"
-        renterCost="$14,292"
-        ownerCost="$29,752"/>
-    </div>
-  );
+const App = () => {
+  const component = new React.Component()
+  
+  component.render = () => {
+    return (
+      <div className="App">
+        <Navbar/>
+        <h1 className="header">Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough</h1>
+  
+        <DataView
+          borough="Brooklyn"
+          renterCost="$14,292"
+          ownerCost="$29,752"/>
+      </div>
+    );
+  }
+  
+  return component
 }
 
 export default App;
